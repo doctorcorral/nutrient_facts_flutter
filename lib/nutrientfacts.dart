30d8b00cca32b22bf7e20fe49af55cbd13c2ee35
Ricardo C <ricardo@suggestic.com>
2018-07-29 19:42:34 -0500

    change dly schema for handling male and female

--- lib/nutrientfacts.dart
+++ lib/nutrientfacts.dart
@@ -49,20 +49,60 @@ Widget nutrientValues({nutrientData}) {
   //final n = (1.3456).toStringAsFixed(2);
   //final s = double.parse("1.2345");
   final nutrientTypes = [
-    {"nutrient": "FAT", "name": "Total Fat", "sub": false, "dly": 65.0},
-    {"nutrient": "SATFAT", "name": "Saturated Fat", "sub": true, "dly": 20.0},
-    {"nutrient": "TRANSFAT", "name": "Trans Fat", "sub": true, "dly": null},
-    {"nutrient": "CHOLE", "name": "Cholesterol", "sub": false, "dly": 300.0},
-    {"nutrient": "NA", "name": "Sodium", "sub": false, "dly": 2400.0},
+    {
+      "nutrient": "FAT",
+      "name": "Total Fat",
+      "sub": false,
+      "dly": {"male": 65.0, "felame": 65.0}
+    },
+    {
+      "nutrient": "SATFAT",
+      "name": "Saturated Fat",
+      "sub": true,
+      "dly": {"male": 20.0, "felame": 20.0}
+    },
+    {
+      "nutrient": "TRANSFAT",
+      "name": "Trans Fat",
+      "sub": true,
+      "dly": {"male": null, "felame": null}
+    },
+    {
+      "nutrient": "CHOLE",
+      "name": "Cholesterol",
+      "sub": false,
+      "dly": {"male": 300.0, "felame": 300.0}
+    },
+    {
+      "nutrient": "NA",
+      "name": "Sodium",
+      "sub": false,
+      "dly": {"male": 2400.0, "felame": 2400.0}
+    },
     {
       "nutrient": "CHOCDF",
       "name": "Total Carbohidrate",
       "sub": false,
-      "dly": 300.0,
+      "dly": {"male": 300.0, "felame": 300.0},
     },
-    {"nutrient": "FIBTG", "name": "Dietary Fiber", "sub": true, "dly": 25.0},
-    {"nutrient": "SUGAR", "name": "Sugars", "sub": true, "dly": null},
-    {"nutrient": "PROCNT", "name": "Protein", "sub": false, "dly": 50.0},
+    {
+      "nutrient": "FIBTG",
+      "name": "Dietary Fiber",
+      "sub": true,
+      "dly": {"male": 25.0, "felame": 25.0}
+    },
+    {
+      "nutrient": "SUGAR",
+      "name": "Sugars",
+      "sub": true,
+      "dly": {"male": null, "felame": null}
+    },
+    {
+      "nutrient": "PROCNT",
+      "name": "Protein",
+      "sub": false,
+      "dly": {"male": 50.0, "felame": 50.0}
+    },
   ];
 
   return Column(
@@ -71,14 +111,14 @@ Widget nutrientValues({nutrientData}) {
         .map((d) => nutrientLiner(
               nutrientName: d["name"],
               qty: nutrientData["${d["nutrient"]}"]["amount"],
-              ptg: d["dly"] != null
+              ptg: d["dly"]["male"] != null
                   ? ((nutrientData["${d["nutrient"]}"]["amount"] * 100) /
-                          d["dly"])
+                          d["dly"]["male"])
                       .toStringAsFixed(2)
                   : "-",
               sub: d["sub"],
               unit: nutrientData["${d["nutrient"]}"]["unit"],
-              showp: d["dly"] != null ? true : false,
+              showp: d["dly"]["male"] != null ? true : false,
             ))
         .toList(),
   );
@@ -88,12 +128,42 @@ Widget vitaminValues({nutrientData}) {
   //final n = (1.3456).toStringAsFixed(2);
   //final s = double.parse("1.2345");
   final nutrientTypes = [
-    {"nutrient": "THIA", "name": "Thiamin", "sub": false, "dly": 1.5},
-    {"nutrient": "K", "name": "Potassium", "sub": false, "dly": 3500.0},
-    {"nutrient": "VITB6A", "name": "Vitamin B6", "sub": false, "dly": 2.0},
-    {"nutrient": "VITA_IU", "name": "Vitamin A", "sub": false, "dly": 5000},
-    {"nutrient": "VITD", "name": "Vitamin D", "sub": false, "dly": 400},
-    {"nutrient": "VITK1", "name": "Vitamin K ", "sub": false, "dly": 80},
+    {
+      "nutrient": "THIA",
+      "name": "Thiamin",
+      "sub": false,
+      "dly": {"male": 1.5, "felame": 1.5}
+    },
+    {
+      "nutrient": "K",
+      "name": "Potassium",
+      "sub": false,
+      "dly": {"male": 3500.0, "felame": 3500.0}
+    },
+    {
+      "nutrient": "VITB6A",
+      "name": "Vitamin B6",
+      "sub": false,
+      "dly": {"male": 2.0, "felame": 2.0}
+    },
+    {
+      "nutrient": "VITA_IU",
+      "name": "Vitamin A",
+      "sub": false,
+      "dly": {"male": 5000, "felame": 5000}
+    },
+    {
+      "nutrient": "VITD",
+      "name": "Vitamin D",
+      "sub": false,
+      "dly": {"male": 400, "felame": 400}
+    },
+    {
+      "nutrient": "VITK1",
+      "name": "Vitamin K ",
+      "sub": false,
+      "dly": {"male": 80, "felame": 80}
+    },
   ];
   final vitaminLine = Container(
       margin: EdgeInsetsDirectional.only(start: 1.0, end: 1.0),
@@ -106,13 +176,13 @@ Widget vitaminValues({nutrientData}) {
             .map((d) => vitaminLiner(
                   nutrientName: d["name"],
                   qty: nutrientData["${d["nutrient"]}"]["amount"],
-                  ptg: d["dly"] != null
+                  ptg: d["dly"]["male"] != null
                       ? ((nutrientData["${d["nutrient"]}"]["amount"] * 100) /
-                              d["dly"])
+                              d["dly"]["male"])
                           .toStringAsFixed(2)
                       : "-",
                   unit: nutrientData["${d["nutrient"]}"]["unit"],
-                  showp: d["dly"] != null ? true : false,
+                  showp: d["dly"]["male"] != null ? true : false,
                 ))
             .toList(),
   );

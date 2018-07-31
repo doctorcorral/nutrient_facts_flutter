864e1f66c9be3f9822f95afac166a1622ef1e30e
Ricardo C <ricardo@suggestic.com>
2018-07-31 18:50:58 -0500

    add metanutrient for storing nutrien metadata

---
+++ lib/metanutrient.dart
@@ -0,0 +1,56 @@
+final macroNutrientTypes = [
+  {
+    "nutrient": "FAT",
+    "name": "Total Fat",
+    "sub": false,
+    "dly": {"male": 65.0, "felame": 65.0}
+  },
+  {
+    "nutrient": "SATFAT",
+    "name": "Saturated Fat",
+    "sub": true,
+    "dly": {"male": 20.0, "felame": 20.0}
+  },
+  {
+    "nutrient": "TRANSFAT",
+    "name": "Trans Fat",
+    "sub": true,
+    "dly": {"male": null, "felame": null}
+  },
+  {
+    "nutrient": "CHOLE",
+    "name": "Cholesterol",
+    "sub": false,
+    "dly": {"male": 300.0, "felame": 300.0}
+  },
+  {
+    "nutrient": "NA",
+    "name": "Sodium",
+    "sub": false,
+    "dly": {"male": 2400.0, "felame": 2400.0}
+  },
+  {
+    "nutrient": "CHOCDF",
+    "name": "Total Carbohidrate",
+    "sub": false,
+    "dly": {"male": 300.0, "felame": 300.0},
+  },
+  {
+    "nutrient": "FIBTG",
+    "name": "Dietary Fiber",
+    "sub": true,
+    "dly": {"male": 25.0, "felame": 25.0}
+  },
+  {
+    "nutrient": "SUGAR",
+    "name": "Sugars",
+    "sub": true,
+    "dly": {"male": null, "felame": null}
+  },
+  {
+    "nutrient": "PROCNT",
+    "name": "Protein",
+    "sub": false,
+    "dly": {"male": 50.0, "felame": 50.0}
+  },
+];

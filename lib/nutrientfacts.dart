2eb900659424c837261c581b55758aa11e6bd615
Ricardo C <ricardo@suggestic.com>
2018-08-03 14:52:45 -0500

    change metanutrient import and access

--- lib/nutrientfacts.dart
+++ lib/nutrientfacts.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nutrientfacts/metanutrient.dart';
 
 class NutritionFacts extends StatefulWidget {
   final nutrientData;
@@ -48,62 +49,7 @@ Widget nutrientWidget({nutrientData}) {
 Widget nutrientValues({nutrientData}) {
   //final n = (1.3456).toStringAsFixed(2);
   //final s = double.parse("1.2345");
-  final nutrientTypes = [
-    {
-      "nutrient": "FAT",
-      "name": "Total Fat",
-      "sub": false,
-      "dly": {"male": 65.0, "felame": 65.0}
-    },
-    {
-      "nutrient": "SATFAT",
-      "name": "Saturated Fat",
-      "sub": true,
-      "dly": {"male": 20.0, "felame": 20.0}
-    },
-    {
-      "nutrient": "TRANSFAT",
-      "name": "Trans Fat",
-      "sub": true,
-      "dly": {"male": null, "felame": null}
-    },
-    {
-      "nutrient": "CHOLE",
-      "name": "Cholesterol",
-      "sub": false,
-      "dly": {"male": 300.0, "felame": 300.0}
-    },
-    {
-      "nutrient": "NA",
-      "name": "Sodium",
-      "sub": false,
-      "dly": {"male": 2400.0, "felame": 2400.0}
-    },
-    {
-      "nutrient": "CHOCDF",
-      "name": "Total Carbohidrate",
-      "sub": false,
-      "dly": {"male": 300.0, "felame": 300.0},
-    },
-    {
-      "nutrient": "FIBTG",
-      "name": "Dietary Fiber",
-      "sub": true,
-      "dly": {"male": 25.0, "felame": 25.0}
-    },
-    {
-      "nutrient": "SUGAR",
-      "name": "Sugars",
-      "sub": true,
-      "dly": {"male": null, "felame": null}
-    },
-    {
-      "nutrient": "PROCNT",
-      "name": "Protein",
-      "sub": false,
-      "dly": {"male": 50.0, "felame": 50.0}
-    },
-  ];
+  final nutrientTypes = MetaDataNutrient.macroNutrientTypes;
 
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
@@ -127,44 +73,7 @@ Widget nutrientValues({nutrientData}) {
 Widget vitaminValues({nutrientData}) {
   //final n = (1.3456).toStringAsFixed(2);
   //final s = double.parse("1.2345");
-  final nutrientTypes = [
-    {
-      "nutrient": "THIA",
-      "name": "Thiamin",
-      "sub": false,
-      "dly": {"male": 1.5, "felame": 1.5}
-    },
-    {
-      "nutrient": "K",
-      "name": "Potassium",
-      "sub": false,
-      "dly": {"male": 3500.0, "felame": 3500.0}
-    },
-    {
-      "nutrient": "VITB6A",
-      "name": "Vitamin B6",
-      "sub": false,
-      "dly": {"male": 2.0, "felame": 2.0}
-    },
-    {
-      "nutrient": "VITA_IU",
-      "name": "Vitamin A",
-      "sub": false,
-      "dly": {"male": 5000, "felame": 5000}
-    },
-    {
-      "nutrient": "VITD",
-      "name": "Vitamin D",
-      "sub": false,
-      "dly": {"male": 400, "felame": 400}
-    },
-    {
-      "nutrient": "VITK1",
-      "name": "Vitamin K ",
-      "sub": false,
-      "dly": {"male": 80, "felame": 80}
-    },
-  ];
+  final nutrientTypes = MetaDataNutrient.vitaminTypes;
   final vitaminLine = Container(
       margin: EdgeInsetsDirectional.only(start: 1.0, end: 1.0),
       height: 4.0,
@@ -176,13 +85,15 @@ Widget vitaminValues({nutrientData}) {
             .map((d) => vitaminLiner(
                   nutrientName: d["name"],
                   qty: nutrientData["${d["nutrient"]}"]["amount"],
-                  ptg: d["dly"]["male"] != null
+                  ptg: (d["dly"] as Map<String, num>)["male"] != null
                       ? ((nutrientData["${d["nutrient"]}"]["amount"] * 100) /
-                              d["dly"]["male"])
+                              (d["dly"] as Map<String, num>)["male"])
                           .toStringAsFixed(2)
                       : "-",
                   unit: nutrientData["${d["nutrient"]}"]["unit"],
-                  showp: d["dly"]["male"] != null ? true : false,
+                  showp: (d["dly"] as Map<String, num>)["male"] != null
+                      ? true
+                      : false,
                 ))
             .toList(),
   );
